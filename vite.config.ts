39fc9c6d10430c8cb85176b19d7a815c8bd8cd80
anthony-propuestas <anthony.propuestas@gmail.com>
2026-04-26 14:50:26 -0300

    error de admij

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   server: {
     allowedHosts: true,
+    proxy: {
+      "/api": "http://localhost:8788",
+    },
   },
   build: {
     chunkSizeWarningLimit: 5000,
